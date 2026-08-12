63d52e673df8e15fb0a7a058ffcfbcd590d8979c
ianshengfong-bit <ianshengfong@gmail.com>
2026-08-12 14:29:49 +0800

    更新回第一版

--- sw.js
+++ sw.js
@@ -1,82 +1,38 @@
-const CACHE_NAME = "yao-fire-v1";
+const CACHE = "yao-pwa-v1";
 
-const FILES = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./app.js",
-    "./manifest.json",
-    "./icon.svg"
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon.svg"
 ];
 
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
 
-self.addEventListener(
-    "install",
-    event => {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
 
-        event.waitUntil(
-
-            caches.open(CACHE_NAME)
-                .then(cache => {
-
-                    return cache.addAll(FILES);
-
-                })
-
-        );
-
-        self.skipWaiting();
-
-    }
-);
-
-
-self.addEventListener(
-    "activate",
-    event => {
-
-        event.waitUntil(
-
-            caches.keys()
-                .then(keys => {
-
-                    return Promise.all(
-
-                        keys
-                            .filter(key =>
-                                key !== CACHE_NAME
-                            )
-                            .map(key =>
-                                caches.delete(key)
-                            )
-
-                    );
-
-                })
-
-        );
-
-        self.clients.claim();
-
-    }
-);
-
-
-self.addEventListener(
-    "fetch",
-    event => {
-
-        event.respondWith(
-
-            caches.match(event.request)
-                .then(cached => {
-
-                    return cached ||
-                        fetch(event.request);
-
-                })
-
-        );
-
-    }
-);
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request);
+    })
+  );
+});
